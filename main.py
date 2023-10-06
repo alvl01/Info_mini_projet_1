7b78b056b39cd01fbd77d58ff6d83d2bf47b42b3
ALESSANDRO <moricialessandro@live.be>
2023-10-06 12:11:12 +0200

    main 1.02

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 
 def launch():
 	print("pour jouer ...")
+## info
+def help_game():
+	pass
+
+def ship_state():
+	pass
+def planet_position():
+	pass
+def get_time():
+	pass
+def planet_resources():
+	pass
+
+## Action
+def move():
+	pass
+
+def upgrade():
+	pass
+
+def repare():
+	pass
