b9d9a95f3cf8d425de9375bbf2d64c7cc56d6239
ALESSANDRO <moricialessandro@live.be>
2023-10-06 13:05:20 +0200

    gaming 1.04
    specification

--- gaming_API_gr_26.py
+++ gaming_API_gr_26.py
@@ -4,6 +4,9 @@ import random.randint
 
 ## info
 def help_game():#jugurtha
+	"""
+	explain how to use the other functions
+	"""
 	print("ship_state affiche ...")
 	print("")
 	print("")
@@ -11,36 +14,101 @@ def help_game():#jugurtha
 	print("")
 	print("")
 	print("")
+	return
 
 def ship_state(ship):#anderson
+	"""
+	diplay information about the ship
+
+	parameters
+	----------
+	ship: name of the ship (str)
+	"""
 	loc = gaming_tools.get_ship_location(ship)
 	speed = gaming_tools.get_ship_speed(ship)
 	broken = gaming_tools.is_ship_broken(ship)
 	ready = gaming_tools.get_when_ship_is_ready(ship)
 	print("")
+	return
 
-def planet_position():#ammar
-	pass
+def planet_position(planet):#ammar
+	"""
+	display planet position
 
-def planet_resources():#abdelaziz
-	pass
+	parameters
+	----------
+	planet: name of the planet (str)
+	"""
+	return
+
+def planet_resources(planet):#abdelaziz
+	"""
+	display the quantity of ressources of a planet
+
+	parameters
+	----------
+	planet: name of the planet (str)
+	"""
+
+	return
 
 ## Action
-def move():#Alessandro
-	pass
+def move(ship):#Alessandro
+	"""
+	move the ship
 
-def upgrade():#abdelaziz
-	pass
+	parameters
+	----------
+	ship: name of the ship (str)
+	"""
 
-def repare():#jugurtha
-	pass
+	return
 
-def create_planet():#anderson
-	pass
+def upgrade(ship):#abdelaziz
+	"""
+	uprade the ship
+	
+	parameters
+	----------
+	ship: name of the ship (str)
+	"""
+	return
 
-def create_ship():#ammar
-	pass
+def repare(ship):#jugurtha
+	"""
+	repare the ship
+
+	parameters
+	----------
+	ship: name of the ship (str)
+	"""
+	return
+
+def create_planet(name):#anderson
+	"""
+	create a planet
+	display a confirmation
+
+	parameters
+	----------
+	name: name of the planet
+	"""
+	return
+
+def create_ship(name):#ammar
+	"""
+	create a ship
+	display a confirmation
+
+	parameters
+	----------
+	name: name of the ship
+	"""
+	return
 
 
 def reset():#Alessandro
-	pass
+	"""
+	reset the game (prépare for a new game)
+	"""
+	return
