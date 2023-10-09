fae429295a508edc0fdedcb7ae99e207dd6d860a
ALESSANDRO <moricialessandro@live.be>
2023-10-09 15:56:59 +0200

    test 1

--- gaming_API_gr_26.py
+++ gaming_API_gr_26.py
@@ -1,20 +1,10 @@
 import gaming_tools
-import time.time
-import random.randint
+import time
+import random
 
 ## info
 def help_game():#jugurtha
 	"""Explain how to use the functions of this game.
-
-	Available functions:
-	1. create_ship(ship_name) - Create a new ship with the given name.
-	2. move_ship(ship_name, destination_planet) - Move the ship to the destination planet.
-	3. repair_ship(ship_name) - Repair a broken ship if enough resources are available.
-	4. planet_exists(planet_name) - Check if a planet with the given name exists.
-	5. add_new_planet(planet_name, resources) - Add a new planet with the specified resources.
-	6. get_distance(ship_name, planet_name) - Get the distance between ship and the planet.")
-	7. planet_state(planet_name) - Display information about the planet
-	8. ship_state(planet_name) - Display information about the ship
 	"""
 	print("Available functions :")
 	print("1. create_planet(planet_name) - Create a new planet with the given name.")
@@ -31,6 +21,13 @@ def help_game():#jugurtha
 	print('move_ship("MyShip", "NewPlanet")')
 	print('\nAnd so on ..')
 
+def init_game():
+
+	create_planet("Aldebaran", 0, 0)
+	create_planet("Epsilon Aurigae", 1000, 1000)
+	gaming_tools.set_planet_resources("Aldebaran", 0)
+	gaming_tools.set_planet_resources("Epsilon Aurigae", 0)
+	print("planet Aldebaran and Epsilon Aurigae have been created and set to 0")
 
 def ship_state(ship):#anderson
 	"""Diplay information about the ship
@@ -46,8 +43,13 @@ def ship_state(ship):#anderson
 	speed = gaming_tools.get_ship_speed(ship)
 	broken = gaming_tools.is_ship_broken(ship)
 	ready = gaming_tools.get_when_ship_is_ready(ship)
-	print("")
+	print("Location %s" % loc)
+	print("Speed: %d parsec/sec" % speed)
+	print(("Ship is broken" if broken else "ship is not broken!"))
+	print("Ship is ready" if ship_is_ready(ship) else "ship is not ready wait " + text_time(ready - time.time()) + "s")
 
+def ship_is_ready(ship):
+	return gaming_tools.get_when_ship_is_ready(ship) < time.time()
 
 # planet ressource et planet postion on ete combiné
 def planet_state(planet):#ammar#abdelaziz
@@ -57,12 +59,12 @@ def planet_state(planet):#ammar#abdelaziz
 	----------
 	planet: name of the planet (str)
 	"""
-	if not gaming_tools.planet_exists(ship):
+	if not gaming_tools.planet_exists(planet):
 		error(2)
 		return
 	location = gaming_tools.get_planet_location(planet)
-	resources = gaming_tools.get_planet_ressources(planet)
-	print("")
+	resources = gaming_tools.get_planet_resources(planet)
+	print("the planet is in location x: %s and have %d resources" %(location, resources))
 
 def get_distance(ship, planet):
 	"""Calculate the distance between ship and planet
@@ -79,10 +81,10 @@ def get_distance(ship, planet):
 	if not gaming_tools.ship_exists(ship):
 		error(1)
 		return
-	if not gaming_tools.planet_exists(ship):
+	if not gaming_tools.planet_exists(planet):
 		error(2)
 		return
-	ship_x, ship_y = gaming_tools.get_ship_location(ship)
+	ship_x, ship_y = gaming_tools.get_planet_location(gaming_tools.get_ship_location(ship))
 	planet_x, planet_y = gaming_tools.get_planet_location(planet)
 	return ((ship_x - planet_x) ** 2 + (ship_y - planet_y) ** 2) ** (1/2)
 
@@ -101,7 +103,7 @@ def text_time(time):
 
 ## Action
 
-def move(ship="", planet=""):#Alessandro
+def move(ship, planet):#Alessandro
 	"""Move the ship
 
 	Parameters
@@ -109,22 +111,16 @@ def move(ship="", planet=""):#Alessandro
 	ship: name of the ship (str)
 	planet: name of planet where you want to go (str)
 	"""
-	if not ship or not planet:
-		error(12)
-		return
-	if type(ship) != str or type(planet) != str:
-		error(11)
-		return
 	if not gaming_tools.ship_exists(ship):
 		error(1)
 		return
-	if not gaming_tools.planet_exists(ship):
+	if not gaming_tools.planet_exists(planet):
 		error(2)
 		return
 	if gaming_tools.is_ship_broken(ship):
 		error(5)
 		return
-	if not gaming_tools.get_when_ship_is_ready(ship):
+	if not ship_is_ready(ship):
 		error(6)
 		return
 	# change position of the ship
@@ -140,12 +136,13 @@ def move(ship="", planet=""):#Alessandro
 	# set waiting time
 	gaming_tools.set_when_ship_is_ready(ship, time.time() + deltatime)
 
-def upgrade_ship(ship):#abdelaziz
+def upgrade_ship(ship, upgrade):#abdelaziz
 	"""Uprade the ship
 	
 	Parameters
 	----------
 	ship: name of the ship (str)
+	upgrade: velocity added (int)
 	"""
 	if not gaming_tools.ship_exists(ship):
 		error(1)
@@ -153,32 +150,36 @@ def upgrade_ship(ship):#abdelaziz
 	if gaming_tools.is_ship_broken(ship):
 		error(7)
 		return
-	if gaming_tools.get_planet_ressources(planet) < upgrade:
+	if not ship_is_ready(ship):
+		error(6)
+		return
+	planet = gaming_tools.get_ship_location(ship)
+	if gaming_tools.get_planet_resources(planet) < upgrade:
 		error(8)
 		return
 	speed = gaming_tools.get_ship_speed(ship) + upgrade
 	gaming_tools.set_ship_speed(ship, speed)
 	upgrade_time = 40 * (upgrade**2)
+	print("Ship is upgrading ... Please wait for %d seconds" % (int(upgrade_time)))
 	gaming_tools.set_when_ship_is_ready(ship, time.time() + upgrade_time)
 
-def repair(ship, planet): #jugurtha
+def repair(ship): #jugurtha
 	"""Repare the ship
 
 	Parameters
 	----------
 	ship: name of the ship (str)
-	planet: name of the current planet (str)
 	"""
 	if not gaming_tools.ship_exists(ship):
 		error(1)
 		return
-	if not gaming_tools.planet_exists(planet):
-		error(2)
-		return
 	if not gaming_tools.is_ship_broken(ship):
 		error(9)
 		return
-	
+	if not ship_is_ready(ship):
+		error(6)
+		return
+	planet = gaming_tools.get_ship_location(ship)
 	planet_resources = gaming_tools.get_planet_resources(planet)
 	if(planet_resources < 3) :
 		error(8)
@@ -186,20 +187,19 @@ def repair(ship, planet): #jugurtha
 	ship_speed = gaming_tools.get_ship_speed(ship) 
 	repaire_time = 20 * ship_speed
 
-	print("Ship is under repair ... Please wait for %f seconds" % (repaire_time))
+	print("Ship is under repair ... Please wait for %d seconds" % (int(repaire_time)))
 	gaming_tools.set_when_ship_is_ready(ship, time.time() + repaire_time)
 
-	print("Ship is now repaired and ready to start")
 	gaming_tools.set_ship_broken(ship, False)
 
-	gaming_tools.set_planet_resources(planet_resources - 3)
+	gaming_tools.set_planet_resources(planet, planet_resources - 3)
 
 
 
 	
        
 
-def create_planet(name="", coord_x=-1, coord_y=-1):#anderson
+def create_planet(name, coord_x, coord_y):#anderson
 	"""Create a planet
 
 	Parameters
@@ -208,12 +208,6 @@ def create_planet(name="", coord_x=-1, coord_y=-1):#anderson
 	coord_x: x coordinate of the planet (int)
 	coord_y: y coordinate of the planet (int)
 	"""
-	if not name:
-		error(12)
-		return
-	if type(name) != str or type(coord_y) != int or type(coord_x) != int:
-		error(11)
-		return
 	if gaming_tools.planet_exists(name):
 		error(4)
 		return
@@ -223,13 +217,9 @@ def create_planet(name="", coord_x=-1, coord_y=-1):#anderson
 
 	ressources = random.randint(5, 20)
 
-	if gaming_tools.add_new_planet(name, ressources):
-		gaming_tools.set_planet_location(name, coord_x, coord_y)
-		print("the ship has been created")
-		return
-    print("it failed!")
-
-	#display a confirmation
+	gaming_tools.add_new_planet(name, ressources)
+	gaming_tools.set_planet_location(name, coord_x, coord_y)
+	print("the planet has been created")
 
 def create_ship(name):#ammar
 	"""Create a ship
@@ -238,13 +228,15 @@ def create_ship(name):#ammar
 	----------
 	name: name of the ship (str)
 	"""
+	if not gaming_tools.planet_exists("Aldebaran"):
+		error(13)
+		return
 	if gaming_tools.ship_exists(name):
 		error(3)
 		return
-	if gaming_tools.add_new_ship(name, 1, False):
-        print("the ship has been created")
-        return
-    print("it failed!")
+	gaming_tools.add_new_ship(name, 1, False)
+	gaming_tools.set_ship_location(name, "Aldebaran")
+	print("the ship has been created")
 
 def error(value):# everyone just add error that you need
 	"""Display error to the user
@@ -274,11 +266,17 @@ def error(value):# everyone just add error that you need
 	elif value == 10:
 		print("Error, the coordinates are not between 0 and 1000!")
 	elif value == 11:
-		print("Error, the type of the parameters did not matches!")
+		print("")
+	elif value == 12:
+		print("Error, you re not on this planet!")
+	elif value == 13:
+		print("Error, Aldebaran is not created yet!")
 
 
 def reset():#Alessandro
 	"""
 	reset the game (prépare for a new game)
 	"""
+	gaming_tools.reset_game()
+	print("The game has been reset!")
 	return
